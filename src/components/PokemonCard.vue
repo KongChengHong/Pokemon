57514e0cfa4458761917eee6e535553ad19e7521
KongChengHong <danid91401@gmail.com>
2019-11-14 01:16:28 +0800

    edit

---
+++ src/components/PokemonCard.vue
@@ -0,0 +1,18 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+data() {
+    return {
+    title : 'Sample pokemon',
+    image : 'https://res.cloudinary.com/mayashavin/image/upload/v1562279388/pokemons/br5xigetaqosvwf1r1eb.jpg',
+    };
+  },
+}
+</script>
+
+<style>
+
+</style>
